d02f4e380ee3cf08035331559cdba95e4bf86268
Stijn Fleuren <stijnfleuren20@hotmail.com>
2020-10-07 12:01:58 +0200

    Additional validation on PhaseDiagram (related) objects.

--- swift_cloud_py/entities/control_output/phase_diagram.py
+++ swift_cloud_py/entities/control_output/phase_diagram.py
@@ -14,8 +14,9 @@ class GreenYellowPhase:
         :param signalgroup_id:
         :param interval_index:
         """
-        self.signalgroup_id = signalgroup_id
-        self.interval_index = interval_index
+        # explicit type conversion ensures correct types are used
+        self.signalgroup_id = str(signalgroup_id)
+        self.interval_index = int(interval_index)
 
     def to_json(self) -> List:
         """get json-serializable structure that can be stored as json with json.dumps()"""
@@ -35,6 +36,8 @@ class Phase:
     def __init__(self, greenyellow_phases: List[GreenYellowPhase]) -> None:
         self.greenyellow_phases = greenyellow_phases
 
+        self._validate()
+
     def to_json(self) -> List[List]:
         """get json-serializable structure that can be stored as json with json.dumps()"""
         return [greenyellow_phase.to_json() for greenyellow_phase in self.greenyellow_phases]
@@ -44,6 +47,15 @@ class Phase:
         """Loading phase from json (expected same json structure as generated with to_json)"""
         return Phase(greenyellow_phases=[GreenYellowPhase.from_json(greenyellow_phase)
                                          for greenyellow_phase in phase_list])
+
+    def _validate(self):
+        """ Validate arguments of Phase object"""
+        error_message = "greenyellow_phases should be a list of GreenYellowPhase-objects"
+        if not isinstance(self.greenyellow_phases, list):
+            raise ValueError(error_message)
+        for greenyellow_phase in self.greenyellow_phases:
+            if not isinstance(greenyellow_phase, GreenYellowPhase):
+                raise ValueError(error_message)
 
     def __str__(self) -> str:
         """string representation of object"""
@@ -63,6 +75,8 @@ class PhaseDiagram:
     def __init__(self, phases: List[Phase]) -> None:
         self.phases = phases
 
+        self._validate()
+
     def to_json(self) -> List[List[List]]:
         """get json_serializable structure that can be stored as json with json.dumps()"""
         return [phase.to_json() for phase in self.phases]
@@ -71,6 +85,15 @@ class PhaseDiagram:
     def from_json(phase_lists: List[List[List]]) -> PhaseDiagram:
         """Loading phase diagram from json (expected same json structure as generated with to_json)"""
         return PhaseDiagram(phases=[Phase.from_json(phase_list=phase_list) for phase_list in phase_lists])
+
+    def _validate(self):
+        """ Validate arguments of PhaseDiagram object"""
+        error_message = "phases should be a list of Phase-objects"
+        if not isinstance(self.phases, list):
+            raise ValueError(error_message)
+        for phase in self.phases:
+            if not isinstance(phase, Phase):
+                raise ValueError(error_message)
 
     def __str__(self) -> str:
         """string representation of object"""
