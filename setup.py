b1ee9ecbb68d6e01fed6a31dbfd791f3da247fdd
Stijn Fleuren <stijnfleuren20@hotmail.com>
2020-10-09 18:49:33 +0200

    Updated setup.py to create version=0.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="swift-cloud-api",
-    version="0.0.4",
+    version="0.0.5",
     author="S.T.G. Fleuren",
     author_email="stijn.fleuren@swiftmobility.eu",
     description="Swift Mobility Cloud API Interface",
