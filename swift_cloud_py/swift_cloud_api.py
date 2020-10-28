37b3b47a5cc01a5102843a90e654b6e1e5d78fa4
Stijn Fleuren <stijnfleuren20@hotmail.com>
2020-10-28 10:48:26 +0100

    Added functionality to tune a fixed-time schedule to a new traffic situation.

--- swift_cloud_py/swift_cloud_api.py
+++ swift_cloud_py/swift_cloud_api.py
@@ -23,6 +23,8 @@ from swift_cloud_py.validate_safety_restrictions.validate import validate_safety
 CLOUD_API_URL = os.environ.get("smc_api_url", "https://cloud-api.swiftmobility.eu")
 
 CONNECTION_ERROR_MSG = "Connection with swift mobility cloud api could not be established"
+HORIZON_LB_EXCEEDED_MSG = "horizon should exceed one hour"
+
 
 def check_status_code(response: Response) -> None:
     """
@@ -64,7 +66,7 @@ class SwiftMobilityCloudApi:
                           horizon: float = 2.0,
                           min_period_duration: float = 0.0, max_period_duration: float = 180,
                           objective: ObjectiveEnum = ObjectiveEnum.min_delay,
-                          initial_queue_lengths: Optional[QueueLengths] = None,
+                          initial_queue_lengths: Optional[QueueLengths] = None
                           ) -> Tuple[FixedTimeSchedule, PhaseDiagram, float]:
         """
         Optimize a fixed-time schedule
@@ -93,7 +95,7 @@ class SwiftMobilityCloudApi:
         (minimized delay, minimized period, or maximum percentual increase in traffic divided by 100, e.g. 1 means
         currently at the verge of stability)
         """
-        assert horizon >= 1, "horizon should exceed one hour"
+        assert horizon >= 1, HORIZON_LB_EXCEEDED_MSG
         if initial_queue_lengths is None:
             # assume no initial traffic
             initial_queue_lengths = QueueLengths({signalgroup.id: [0] * len(signalgroup.traffic_lights)
@@ -138,6 +140,86 @@ class SwiftMobilityCloudApi:
     @classmethod
     @ensure_has_internet
     @authenticate
+    def get_tuned_fts(cls, intersection: Intersection, arrival_rates: ArrivalRates,
+                      fixed_time_schedule: FixedTimeSchedule, horizon: float = 2.0,
+                      min_period_duration: float = 0.0, max_period_duration: float = 180,
+                      objective: ObjectiveEnum = ObjectiveEnum.min_delay,
+                      initial_queue_lengths: Optional[QueueLengths] = None) \
+            -> Tuple[FixedTimeSchedule, float]:
+        """
+        Tune a fixed-time schedule; tune the greenyellow times to a new situation but keep the 'structure' of this
+         fixed-time schedule the same (the phase diagram remains the same).
+        :param intersection: intersection for which to optimize the fts (contains signal groups, conflicts and more)
+        :param arrival_rates: arrival rates; each arrival rate is specified in personal car equivalent per hour (PCE/h)
+        cyclists per hour or pedestrians per hour
+        :param fixed_time_schedule: fixed-time schedule to tune.
+        :param horizon: time period of interest in hours.
+        :param min_period_duration: minimum period duration of the fixed-time schedule in seconds
+        :param max_period_duration: minimum period duration of the fixed-time schedule in seconds
+        :param objective: what kpi (key performance indicator) to optimize. The following options are available:
+         - ObjectiveEnum.min_delay: minimize the delay experienced by road users arriving at the intersection during
+         the next 'horizon' hours. The initially waiting traffic is modeled as implicitly by increasing the
+         arrival rate by initial_queue_length / horizon PCE/h; this implies that we assume that this traffic is arriving
+         (evenly spread) during the horizon.
+         - ObjectiveEnum.min_period: search for the fixed-time schedule with the smallest period duration for which
+         all traffic lights are 'stable', i.e., the greenyellow interval is large enough so that the amount of traffic
+          that  can (on average) depart during the horizon exceeds the traffic that arrives during
+          the horizon (+ initially waiting traffic).
+         - ObjectiveEnum.max_capacity: search for the fixed-time schedule that can handle the largest (percentual)
+         increase in traffic (including the initial amount of traffic), i.e., the largest percentual increase in traffic
+          for which all traffic lights are 'stable' (see also ObjectiveEnum.min_period).
+        :param initial_queue_lengths: initial amount of traffic waiting at each of the traffic lights; if None, then we
+        assume no initial traffic. The unit of each queue-length should align with the unit used for the arrival rate;
+        if the arrival rate is specified in PCE/h then the queue-length needs to be specified in PCE.
+        :return: fixed-time schedule, associated phase diagram and the objective value
+        (minimized delay, minimized period, or maximum percentual increase in traffic divided by 100, e.g. 1 means
+        currently at the verge of stability)
+        """
+        assert horizon >= 1, HORIZON_LB_EXCEEDED_MSG
+        if initial_queue_lengths is None:
+            # assume no initial traffic
+            initial_queue_lengths = QueueLengths({signalgroup.id: [0] * len(signalgroup.traffic_lights)
+                                                  for signalgroup in intersection.signalgroups})
+
+        check_all_arrival_rates_and_queue_lengths_specified(intersection=intersection, arrival_rates=arrival_rates,
+                                                            initial_queue_lengths=initial_queue_lengths)
+
+        endpoint = f"{CLOUD_API_URL}/fts-tuning"
+        headers = SwiftMobilityCloudApi.get_authentication_header()
+
+        # rest-api call
+        try:
+            # assume that the traffic that is initially present arrives during the horizon.
+            corrected_arrival_rates = arrival_rates + initial_queue_lengths / horizon
+            json_dict = dict(
+                intersection=intersection.to_json(),
+                fixed_time_schedule=fixed_time_schedule.to_json(),
+                arrival_rates=corrected_arrival_rates.to_json(),
+                min_period_duration=min_period_duration,
+                max_period_duration=max_period_duration,
+                objective=objective.value
+            )
+            logging.debug(f"calling endpoint {endpoint}")
+            r = requests.post(endpoint, json=json_dict, headers=headers)
+            logging.debug(f"finished calling endpoint {endpoint}")
+        except requests.exceptions.ConnectionError:
+            raise UnknownCloudException(CONNECTION_ERROR_MSG)
+
+        # check for errors
+        check_status_code(response=r)
+
+        # parse output
+        output = r.json()
+        objective_value = output["obj_value"]
+        fixed_time_schedule = FixedTimeSchedule.from_json(output["fixed_time_schedule"])
+        # check if safety restrictions are satisfied; raises a SafetyViolation-exception if this is not the case.
+        validate_safety_restrictions(intersection=intersection, fixed_time_schedule=fixed_time_schedule)
+
+        return fixed_time_schedule, objective_value
+
+    @classmethod
+    @ensure_has_internet
+    @authenticate
     def evaluate_fts(cls, intersection: Intersection, arrival_rates: ArrivalRates,
                      fixed_time_schedule: FixedTimeSchedule, horizon: float = 2.0,
                      initial_queue_lengths: Optional[QueueLengths] = None) -> KPIs:
@@ -153,7 +235,7 @@ class SwiftMobilityCloudApi:
         :param horizon: time period of interest in hours.
         :return KPIs, which are the estimated
         """
-        assert horizon >= 1, "horizon should exceed one hour"
+        assert horizon >= 1, HORIZON_LB_EXCEEDED_MSG
         if initial_queue_lengths is None:
             # assume no initial traffic
             initial_queue_lengths = QueueLengths({signalgroup.id: [0] * len(signalgroup.traffic_lights)
